a8449d1dd228d7ae565a3fec2cba9e3986859b33
ZubsZero <zubairmatthee262@gmail.com>
2023-09-06 09:55:14 +0200

    frontend

---
+++ src/views/HomeView.vue
@@ -0,0 +1,17 @@
+<template>
+   <NavBar/>
+   <Hero/>
+   <carousel/>
+   <Body/>
+</template>
+
+<script>
+import NavBar from '@/components/Navbar-comp.vue'
+import Hero from '@/components/Hero-comp.vue'
+import carousel from '@/components/Carousel-comp.vue'
+import Body from '@/components/Homebody-comp.vue'
+export default {
+  name: "Nav",
+  components: {NavBar,Hero, carousel, Body}
+}
+</script>
